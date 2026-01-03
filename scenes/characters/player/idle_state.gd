2dff2fa2a5e96b190630c3cdd980912b17593703
Angel Madrigal <angelrmadrigal2009@gmail.com>
2026-01-03 13:52:54 -0800

    added player movement, idle and the transitions between them using a state machine

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,20 +1,27 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
-var direction: Vector2
-
-func _on_process(_delta : float) -> void:
-	pass
-
-
 func _on_physics_process(_delta : float) -> void:
-	pass
+	if player.player_direction == Vector2.UP:
+		animated_sprite_2d.play("idle_back")
+	elif player.player_direction == Vector2.RIGHT:
+		animated_sprite_2d.play("idle_right")
+	elif player.player_direction == Vector2.DOWN:
+		animated_sprite_2d.play("idle_front")
+	elif player.player_direction == Vector2.LEFT:
+		animated_sprite_2d.play("idle_left")
+	else:
+		animated_sprite_2d.play("idle_front")
+			
 
 
 func _on_next_transitions() -> void:
-	pass
+	GameInputEvents.movement_input()
+		
+	if GameInputEvents.is_movement_input():
+		transition.emit("Walk")
 
 
 func _on_enter() -> void:
@@ -22,4 +29,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
