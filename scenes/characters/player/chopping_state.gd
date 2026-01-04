571726bb907381d4c37560a764b7b0cf53563aeb
Angel Madrigal <angelrmadrigal2009@gmail.com>
2026-01-03 23:37:35 -0800

    added choppable trees and log collectables

--- scenes/characters/player/chopping_state.gd
+++ scenes/characters/player/chopping_state.gd
@@ -2,6 +2,11 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
+
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0,0);
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -18,16 +23,23 @@ func _on_next_transitions() -> void:
 
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
+		hit_component_collision_shape.position = Vector2(0,-18)
 		animated_sprite_2d.play("chopping_back")
 	elif player.player_direction == Vector2.DOWN:
+		hit_component_collision_shape.position = Vector2(0,3)
 		animated_sprite_2d.play("chopping_front")
 	elif player.player_direction == Vector2.RIGHT:
+		hit_component_collision_shape.position = Vector2(9,0)
 		animated_sprite_2d.play("chopping_right")
 	elif player.player_direction == Vector2.LEFT:
+		hit_component_collision_shape.position = Vector2(-9,0)
 		animated_sprite_2d.play("chopping_left")
 	else:
+		hit_component_collision_shape.position = Vector2(0,3)
 		animated_sprite_2d.play("chopping_front")
-
+		
+	hit_component_collision_shape.disabled = false
 
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
+	hit_component_collision_shape.disabled = true
